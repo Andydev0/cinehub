05f2ab0022e3ab1fd7a35ad09bc5b740aad0df25
Anderson Silva <uvvanderson@gmail.com>
2025-08-07 12:03:02 -0300

    ajustes do reactnode

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import type { ReactNode } from 'react';
 import api from '../services/api';
 
 // Define a "forma" do nosso contexto
